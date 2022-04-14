ec5b5ea69f26296efe57ab1918613f431a38097b
Ben Delany <bendelany693@gmail.com>
2022-04-14 10:38:42 -0400

    Update nums

--- generate_nums.py
+++ generate_nums.py
@@ -10,8 +10,6 @@ with open('/Users/bendelany/workspace/circle/build/circuitgame.github.io/nums.js
     myfile.write(newData)
     myfile.close()
 
-exit()
-
 full_local_path = "/Users/bendelany/workspace/circle/build/circuitgame.github.io"
 username = "circuitgame"
 password = "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"
--- nums.js
+++ nums.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const ANSWER = ['127','176']
-=======
-export const ANSWER = ['305','99']
->>>>>>> d44df43f0bd3eaab046b781e905f1326093e2c17
+export const ANSWER = ['48','14']
